fa897c052a8b6f474835d767b17af0ee14317b6e
Gary O'Neall <gary@sourceauditor.com>
2018-02-05 11:56:10 -0800

    Add a real main method
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- src/org/spdx/licenselistpublisher/Main.java
+++ src/org/spdx/licenselistpublisher/Main.java
@@ -5,6 +5,7 @@
 
 package org.spdx.licenselistpublisher;
 
+import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * Dispatch to the individual tools
@@ -12,8 +13,27 @@ package org.spdx.licenselistpublisher;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out
-				.println("Hello World");
 
+		if (args.length < 1) {
+			usage();
+			return;
+		}
+
+		String spdxTool = args[0];
+		args = ArrayUtils.removeElement(args, args[0]);
+		
+		if ("LicenseListPublisher".equals(spdxTool)) {
+			LicenseListPublisher.main(args);
+		} else if ("LicenseRDFAGenerator ".equals(spdxTool)) {
+			LicenseRDFAGenerator.main(args);
+		} else {
+			usage();
+		}
+	}
+	
+	private static void usage() {
+		System.out.println("Usage: java -jar spdx-tools-jar-with-dependencies.jar <function> <parameters>");
+		System.out.println("LicenseRDFAGenerator - Generates license data");
+		System.out.println("LicensListPublisher - Publishes license list to git data repository");		
 	}
 }
